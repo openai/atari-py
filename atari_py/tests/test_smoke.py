e8a5dd7906a55e48110b3889cf12b69f557f13cf
Jonas Schneider <mail@jonasschneider.com>
2016-04-27 18:58:38 -0700

    Extend the smoke test to include screen capture

--- atari_py/tests/test_smoke.py
+++ atari_py/tests/test_smoke.py
@@ -1,8 +1,16 @@
 import atari_py
+import numpy as np
 
 def test_smoke():
     pong_path = atari_py.get_game_path('pong')
     ale = atari_py.ALEInterface()
     ale.loadROM(pong_path)
     action_set = ale.getMinimalActionSet()
+
+    # Test stepping
     ale.act(action_set[0])
+
+    # Test screen capture
+    (screen_width,screen_height) = ale.getScreenDims()
+    arr = np.zeros((screen_height, screen_width, 4), dtype=np.uint8)
+    ale.getScreenRGB(arr)
