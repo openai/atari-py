d1cb8e99f53654938ae2e3680fd5125679530c96
Greg Brockman <gdb@gregbrockman.com>
2016-05-07 22:32:02 -0700

    Build on more cores

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import os
 from setuptools import setup
 import subprocess
@@ -9,13 +10,15 @@ with open(os.path.join(os.path.dirname(__file__), 'atari_py/package_data.txt')) 
 
 class Build(DistutilsBuild):
     def run(self):
+        cores_to_use = max(1, multiprocessing.cpu_count() - 1)
+        cmd = ['make', 'build', '-C', 'atari_py/ale_interface', '-j', str(cores_to_use)]
         try:
-            subprocess.check_call(['make', 'build', '-C', 'atari_py/ale_interface'])
+            subprocess.check_call(cmd)
         except subprocess.CalledProcessError as e:
             sys.stderr.write("Could not build atari-py: %s. (HINT: are you sure cmake is installed? You might also be missing a library. Atari-py requires: zlib [installable as 'apt-get install zlib1g-dev' on Ubuntu].)\n" % e)
             raise
         except OSError as e:
-            sys.stderr.write("Unable to execute 'make build -C atari_py/ale_interface'. HINT: are you sure `make` is installed?\n")
+            sys.stderr.write("Unable to execute '{}'. HINT: are you sure `make` is installed?\n".format(' '.join(cmd)))
             raise
         DistutilsBuild.run(self)
 
