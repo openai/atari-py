961b5bc4f8ea0dd39cc41fc3de88017a4d1b3560
Greg Brockman <gdb@gregbrockman.com>
2016-05-07 22:32:21 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class Build(DistutilsBuild):
         DistutilsBuild.run(self)
 
 setup(name='atari-py',
-      version='0.0.17',
+      version='0.0.18',
       description='Python bindings to Atari games',
       url='https://github.com/openai/atari-py',
       author='OpenAI',
