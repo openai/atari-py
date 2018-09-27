546681e139e8a416d96e0de79de87dcf06628f17
Peter Zhokhov <peterz@openai.com>
2018-09-27 15:37:26 -0700

    osx specify platform

--- osx_install.sh
+++ osx_install.sh
@@ -6,6 +6,7 @@ pip3 install pytest
 pip3 install .
 pytest .
 
-pip3 wheel . --wheel-dir=/tmp/wheelhouse
+PYPLATFORM=`python3 -c "import distutils.util; print(distutils.util.get_platform())"`
+pip3 wheel . --wheel-dir=/tmp/wheelhouse --build-option --plat-name=$PYPLATFORM
 ls -lht /tmp/wheelhouse
 
