fbdd3eb2ccf6ad46b1063f1ed66c7abd1445f6ce
Peter Zhokhov <peterz@openai.com>
2018-09-27 13:37:50 -0700

    try building a wheel on osx

--- osx_install.sh
+++ osx_install.sh
@@ -1,5 +1,10 @@
 set -ex
-brew install cmake
+# already present in Travis
+# brew install cmake
+
 pip3 install pytest
 pytest .
+
+pip wheel . --wheel-dir=/tmp/wheelhouse
+ls -lht /tmp/wheelhouse
 
