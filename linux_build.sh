9352747efd0dd246d6a4701ddf6b00a71061c570
Peter Zhokhov <peterz@openai.com>
2018-09-28 13:53:04 -0700

    deploying via script

--- linux_build.sh
+++
@@ -1,6 +0,0 @@
-set -ex
-# already present in Travis
-# brew install cmake
-
-PYPLATFORM=`python -c "import distutils.util; print(distutils.util.get_platform())"`
-pip wheel . --wheel-dir=/tmp/wheelhouse --build-option --plat-name=$PYPLATFORM
