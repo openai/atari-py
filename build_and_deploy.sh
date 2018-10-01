386208d52ea40692b877f2dd6b011f6efc7e6edc
Peter Zhokhov <peterz@openai.com>
2018-10-01 14:14:42 -0700

    remove explict tag in build and deploy

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -8,7 +8,6 @@ PYPLATFORM=`python3 -c "import distutils.util; print(distutils.util.get_platform
 pip3 wheel . --wheel-dir=/tmp/wheelhouse --build-option --plat-name=$PYPLATFORM
 ls -lht /tmp/wheelhouse
 
-export TRAVIS_TAG="0.1.2"
 if [[ ! -z "$TRAVIS_TAG" ]]; then
     pip3 install --user twine
     python3 -m twine upload /tmp/wheelhouse/atari_py-*
