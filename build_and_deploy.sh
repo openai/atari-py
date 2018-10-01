e9814838c0f53599b6789da8b461dd5f54b669b4
Peter Zhokhov <peterz@openai.com>
2018-10-01 14:01:42 -0700

    try with set TRAVIS_TAG

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -8,6 +8,7 @@ PYPLATFORM=`python3 -c "import distutils.util; print(distutils.util.get_platform
 pip3 wheel . --wheel-dir=/tmp/wheelhouse --build-option --plat-name=$PYPLATFORM
 ls -lht /tmp/wheelhouse
 
+export TRAVIS_TAG="0.1.2"
 if [[ ! -z "$TRAVIS_TAG" ]]; do
     pip3 install twine
     twine upload /tmp/wheelhouse/atari_py-*
