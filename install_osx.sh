a93fa48a77f05d0b12f7a891ee22c959fd24e852
Peter Zhokhov <peterz@openai.com>
2018-10-02 12:38:57 -0700

    pip and pip3

--- install_osx.sh
+++ install_osx.sh
@@ -4,3 +4,5 @@ brew install wget
 wget https://repo.continuum.io/miniconda/Miniconda3-${PYTHON_VER}-MacOSX-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
+ln -s $HOME/miniconda/bin/pip $HOME/miniconda/bin/pip3
+ln -s $HOME/miniconda/bin/python $HOME/miniconda/bin/python3
