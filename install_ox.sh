15c1f5547f9cba5ec4c537b3dab0b4f96b26fcda
Peter Zhokhov <peterz@openai.com>
2018-10-02 12:23:23 -0700

    try with miniconda on osx

---
+++ install_ox.sh
@@ -0,0 +1,5 @@
+PYTHON_VER=3.7.0
+brew install wget
+wget https://repo.continuum.io/miniconda/Miniconda3-${PYTHON_VER}-MacOSX-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
