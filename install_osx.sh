e24b42d04fd5d9e1d916d6fac65680c734e879ff
Peter Zhokhov <peterz@openai.com>
2018-10-02 14:37:11 -0700

    polishing installation script

--- install_osx.sh
+++ install_osx.sh
@@ -3,5 +3,7 @@ CONDA_VER=3.7.0
 curl https://repo.continuum.io/miniconda/Miniconda3-${CONDA_VER}-MacOSX-x86_64.sh -o ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
+conda install pip
 python --version
 python3 --version
+pip --version
