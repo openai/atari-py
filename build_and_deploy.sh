be567ae428d7b2672c0e2c92df7ef36451ac3c0c
Peter Zhokhov <peterz@openai.com>
2018-10-02 13:26:57 -0700

    polishing install scripts

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,17 +1,16 @@
 set -ex
 
-pip3 install pytest
-pip3 install .
-pytest .
+python3 -m pip install pytest
+python3 -m pip install .
+python3 -m pytest .
 
 if [[ ! -z "$TRAVIS_TAG" ]]; then
     
     PYPLATFORM=$(python3 get_platform.py)
-    pip3 wheel . --wheel-dir=/tmp/wheelhouse --build-option --plat-name=$PYPLATFORM
-    ls -lht /tmp/wheelhouse
-
-    pip3 install --user twine
+    python3 -m pip install --user twine
     python3 -m twine upload /tmp/wheelhouse/atari_py-*
+
+    python3 setup.py bdist_wheel --plat-name=$PYPLATFORM
 
     if [[ ! -z "$DEPLOY_SDIST" ]]; then
         python3 setup.py sdist       
--- install_osx.sh
+++ install_osx.sh
@@ -2,6 +2,5 @@ set -ex
 CONDA_VER=3.7.0
 curl https://repo.continuum.io/miniconda/Miniconda3-${CONDA_VER}-MacOSX-x86_64.sh -o ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
-ln -s $HOME/miniconda/bin/pip $HOME/miniconda/bin/pip3
 export PATH="$HOME/miniconda/bin:$PATH"
 python --version
