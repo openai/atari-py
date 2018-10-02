870eeb8eaddbf6a4663005a7e2998210de473a37
Peter Zhokhov <peterz@openai.com>
2018-10-02 13:42:11 -0700

    polishing installation scripts

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,20 +1,20 @@
 set -ex
 
-python3 -m pip install pytest
-python3 -m pip install .
-python3 -m pytest .
+pip install pytest
+pip install .
+pytest .
 
 if [[ ! -z "$TRAVIS_TAG" ]]; then
     
     PYPLATFORM=$(python3 get_platform.py)
-    python3 -m pip install --user twine
-    python3 -m twine upload /tmp/wheelhouse/atari_py-*
+    pip install --user twine
+    python -m twine upload /tmp/wheelhouse/atari_py-*
 
-    python3 setup.py bdist_wheel --plat-name=$PYPLATFORM
+    python setup.py bdist_wheel --plat-name=$PYPLATFORM
 
     if [[ ! -z "$DEPLOY_SDIST" ]]; then
-        python3 setup.py sdist       
-        python3 -m twine upload dist/*
+        python setup.py sdist       
+        python -m twine upload dist/*
     fi
 fi
 
