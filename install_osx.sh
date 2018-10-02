a3c69f769c03c2eef6a17b9962f5816f6f113f6a
Peter Zhokhov <peterz@openai.com>
2018-10-02 12:44:30 -0700

    use curl instead of wget

--- install_osx.sh
+++ install_osx.sh
@@ -1,7 +1,6 @@
 set -ex
 PYTHON_VER=3.7.0
-brew install wget
-wget https://repo.continuum.io/miniconda/Miniconda3-${PYTHON_VER}-MacOSX-x86_64.sh -O ~/miniconda.sh
+curl https://repo.continuum.io/miniconda/Miniconda3-${PYTHON_VER}-MacOSX-x86_64.sh -o ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 ln -s $HOME/miniconda/bin/pip $HOME/miniconda/bin/pip3
