0adee3b73c1a9ce57f1ce31415144f43c751e252
Peter Zhokhov <peterz@openai.com>
2018-10-02 14:44:36 -0700

    fix upload section of the script

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -8,13 +8,11 @@ if [[ ! -z "$TRAVIS_TAG" ]]; then
     
     PYPLATFORM=$(python3 get_platform.py)
     pip install twine
-    twine upload /tmp/wheelhouse/atari_py-*
-
     python setup.py bdist_wheel --plat-name=$PYPLATFORM
 
     if [[ ! -z "$DEPLOY_SDIST" ]]; then
         python setup.py sdist       
-        twine upload dist/*
     fi
+    twine upload dist/*
 fi
 
