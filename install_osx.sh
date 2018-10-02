1f24f01dba6d4d137d1a256d03155d09c7dce9ae
Peter Zhokhov <peterz@openai.com>
2018-10-02 13:22:06 -0700

    tests with python3

--- install_osx.sh
+++ install_osx.sh
@@ -1,7 +1,7 @@
 set -ex
-PYTHON_VER=3.7.0
-curl https://repo.continuum.io/miniconda/Miniconda3-${PYTHON_VER}-MacOSX-x86_64.sh -o ~/miniconda.sh
+CONDA_VER=3.7.0
+curl https://repo.continuum.io/miniconda/Miniconda3-${CONDA_VER}-MacOSX-x86_64.sh -o ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
 ln -s $HOME/miniconda/bin/pip $HOME/miniconda/bin/pip3
+export PATH="$HOME/miniconda/bin:$PATH"
 python --version
