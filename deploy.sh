43340ea0ca37e4568f58c49474a8dbbfef38be1f
Peter Zhokhov <peterz@openai.com>
2019-05-22 11:06:08 -0700

    deploy to s3?

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 set -ex
 if [[ ! -z "$TRAVIS_TAG" ]]; then
     pip install twine
-    twine upload --verbose wheelhouse/atari_py*
+   twine upload --verbose wheelhouse/atari_py*
 
     if [[ ! -z "$DEPLOY_SDIST" ]]; then
         python setup.py sdist
