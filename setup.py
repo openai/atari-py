e9c01e35992c1f76ad4fc87008229ebf84e9ed29
Peter Zhokhov <peterz@openai.com>
2019-05-23 10:50:13 -0700

    do not use make

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ class Build(build_ext):
             # silly patch to disable build steps on windows, as we are building externally
             return
         cores_to_use = max(1, multiprocessing.cpu_count() - 1)
-        cmd = ['make', 'build', '-j', str(cores_to_use)]
         try:
-            cwd = os.path.join(self.build_lib, 'atari_py', 'ale_interface')
-            # cwd = os.path.join('atari_py', 'ale_interface')
-            subprocess.check_call(cmd, cwd=cwd)
+            cwd = os.path.join(self.build_lib, 'atari_py', 'ale_interface', 'build')
+            os.makedirs(cwd, exist_ok=True)
+            subprocess.check_call(['cmake', '..'], cwd=cwd)
+            subprocess.check_call(['cmake', '--build', '..'], cwd=cwd)
         except subprocess.CalledProcessError as e:
             sys.stderr.write("Could not build atari-py: %s. (HINT: are you sure cmake is installed? You might also be missing a library. Atari-py requires: zlib [installable as 'apt-get install zlib1g-dev' on Ubuntu].)\n" % e)
             raise
