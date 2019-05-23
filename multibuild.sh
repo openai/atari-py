61acae70fce93e8db068fa4a8c379a6691739bda
Peter Zhokhov <peterz@openai.com>
2019-05-22 21:23:14 -0700

    fix multibuild.sh script

--- multibuild.sh
+++ multibuild.sh
@@ -5,8 +5,8 @@ export PLAT=x86_64
 export MB_PYTHON_VERSION=$PY_VER
 
 git clone https://github.com/matthew-brett/multibuild && cd multibuild && git checkout 254ad28 && cd ..
-". multibuild/common_utils.sh"
-". multibuild/travis_steps.sh"
+source multibuild/common_utils.sh
+source multibuild/travis_steps.sh
 before_install
 build_wheel $REPO_DIR $PLAT
 install_run $PLAT
