35f8d2b0fcb02f67bec129a560db4de988e548b2
Peter Zhokhov <peterz@openai.com>
2019-05-23 16:50:24 -0700

    use virtualenv in deployment script

--- deploy_to_pypi.sh
+++ deploy_to_pypi.sh
@@ -6,6 +6,10 @@ if [[ -z "$TRAVIS_TAG" ]]; then
     exit 0
 fi
 
+pip install virtualenv --user
+python -m virtualenv --python=python3 .venv
+source .venv/bin/activate
+
 pip install awscli twine
 mkdir -p wheelhouse
 aws s3 cp s3://games-wheels/atari-py/atari_py-${TRAVIS_TAG}-*
