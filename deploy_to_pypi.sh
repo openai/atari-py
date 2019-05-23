3761ac3a07d63ea9303df6ad5a4fb5bc15c883be
Peter Zhokhov <peterz@openai.com>
2019-05-23 16:52:12 -0700

    fake the tag; also fix the script download part

--- deploy_to_pypi.sh
+++ deploy_to_pypi.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+TRAVIS_TAG=0.1.12
+
 if [[ -z "$TRAVIS_TAG" ]]; then
     echo "Not a tagged commit, quitting"
     exit 0
@@ -12,5 +14,5 @@ source .venv/bin/activate
 
 pip install awscli twine
 mkdir -p wheelhouse
-aws s3 cp s3://games-wheels/atari-py/atari_py-${TRAVIS_TAG}-*
+aws s3 cp s3://games-wheels/atari-py/atari_py-${TRAVIS_TAG}-* wheelhouse/
 twine upload --verbose wheelhouse/atari_py*
