a578d2e27558739693f23770c60495c074536462
Peter Zhokhov <peterz@openai.com>
2019-05-23 11:42:03 -0700

    more cleanups

--- win_build.sh
+++ win_build.sh
@@ -9,9 +9,9 @@ mkdir -p atari_py/ale_interface/build
 cd atari_py/ale_interface/build
 cmake -DCMAKE_GENERATOR_PLATFORM=x64 ..
 cmake --build .
+cp Debug/ale_c.dll .
 cd ../../../
 
-cp atari_py/ale_interface/build/Debug/ale_c.dll atari_py/ale_interface/ale_c.dll
 pip install wheel && pip wheel . -w wheelhouse --no-deps -vvv
 ls wheelhouse/atari_py* 
 rm -rf atari_py*
