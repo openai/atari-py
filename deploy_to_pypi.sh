5654047c7a7d136f5d6deeda50b4d06e8c28da27
Peter Zhokhov <peterz@openai.com>
2019-05-23 18:16:57 -0700

    deploy only existing files

--- deploy_to_pypi.sh
+++ deploy_to_pypi.sh
@@ -17,13 +17,14 @@ mkdir -p wheelhouse
 env
 # ugh something in awscli does not work on travis we'll download objects via curl
 # aws s3 cp --recursive s3://games-wheels/atari-py/${TRAVIS_TAG} wheelhouse/
-SUFFIXES="-manylinux1_x86_64.whl -win_amd64.whl -macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl"
-PLATFORMS="-cp35-cp35m -cp36-cp36m -cp37-cp37m"
+# SUFFIXES="-manylinux1_x86_64.whl -win_amd64.whl -macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl"
+SUFFIXES="-manylinux1_x86_64.whl"
+PY_VERS="-cp35-cp35m -cp36-cp36m -cp37-cp37m"
 URLPREFIX="https://s3-us-west-2.amazonaws.com/games-wheels/atari-py/${TRAVIS_TAG}/atari_py-${TRAVIS_TAG}"
 
 cd wheelhouse
 for s in $SUFFIXES; do
-  for p in $PLATFORMS; do
+  for p in $PY_VERS; do
     curl -O ${URLPREFIX}${p}${s}
   done;
 done
