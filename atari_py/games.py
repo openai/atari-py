c78e91baf8f8b579fbbbf699e3747e897a742f25
Christopher Hesse <christopherhesse@users.noreply.github.com>
2019-07-19 16:41:55 -0700

    check for atari_py_roms package

--- atari_py/games.py
+++ atari_py/games.py
@@ -3,15 +3,23 @@ import os
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 
-def games_dir():
-    return os.path.join(SCRIPT_DIR, "atari_roms")
+try:
+    import atari_py_roms
+    _games_dir = os.path.join(atari_py_roms.__path__[0], "atari_roms")
+except ImportError:
+    _games_dir = os.path.join(SCRIPT_DIR, "atari_roms")
+
+
+def get_games_dir():
+    return _games_dir
+
 
 def get_game_path(game_name):
-    path = os.path.join(games_dir(), game_name) + ".bin"
+    path = os.path.join(_games_dir, game_name) + ".bin"
     if not os.path.exists(path):
         raise Exception('ROM is missing for %s, see https://github.com/openai/atari-py#roms for instructions' % (game_name,))
     return path
 
 def list_games():
-    files = os.listdir(games_dir())
+    files = os.listdir(_games_dir)
     return [os.path.basename(f).split(".")[0] for f in files]
