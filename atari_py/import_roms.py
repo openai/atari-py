dc270e059a5d5f4df403b8aea720c6306c0b2805
Christopher Hesse <christopherhesse@users.noreply.github.com>
2019-08-16 18:40:50 -0700

    patch road runner rom to get correct one

--- atari_py/import_roms.py
+++ atari_py/import_roms.py
@@ -3,6 +3,7 @@ import hashlib
 import shutil
 import zipfile
 import argparse
+import io
 
 from .games import get_games_dir
 
@@ -45,10 +46,23 @@ def import_roms(dirpath="."):
 
     def save_if_matches(f):
         hexdigest = _calc_md5(f)
+        if hexdigest == "ce5cc62608be2cd3ed8abd844efb8919":
+            # the ALE version of road_runner.bin is not easily available
+            # patch this file instead to match the correct data
+            delta = {4090: 216, 4091: 111, 4092: 216, 4093: 111, 4094: 216, 4095: 111, 8186: 18, 8187: 43, 8188: -216, 8189: 49, 8190: -216, 8191: 49, 12281: 234, 12282: 18, 12283: 11, 12284: -216, 12285: 17, 12286: -216, 12287: 17, 16378: 18, 16379: -21, 16380: -216, 16381: -15, 16382: -216, 16383: -15}
+            f.seek(0)
+            data = bytearray(f.read())
+            for index, offset in delta.items():
+                data[index] += offset
+            name = f"patched version of {f.name}"
+            f = io.BytesIO(bytes(data))
+            f.name = name
+            hexdigest = _calc_md5(f)
+
         if hexdigest in md5s and hexdigest not in copied_md5s:
             copied_md5s.add(hexdigest)
             rom_path = md5s[hexdigest]
-            print(f"copying {os.path.basename(rom_path)} from {f.name}")
+            print(f"copying {os.path.basename(rom_path)} from {f.name} to {rom_path}")
             os.makedirs(os.path.dirname(rom_path), exist_ok=True)
             f.seek(0)
             with open(rom_path, "wb") as out_f:
