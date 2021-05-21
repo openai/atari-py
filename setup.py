02fdb30abb0b3322cf548f5da6bf90c791092801
Peter Zhokhov <peterz@openai.com>
2021-05-20 18:10:11 -0700

    bump version to 0.2.9

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class CMakeExtension(Extension):
         self.sourcedir = os.path.abspath(sourcedir)
 
 setup(name='atari-py',
-      version='0.2.8',
+      version='0.2.9',
       description='Python bindings to Atari games',
       url='https://github.com/openai/atari-py',
       author='OpenAI',
