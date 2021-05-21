e9e540e82cfec6b8e8db58040839e009b1310261
Peter Zhokhov <peterz@openai.com>
2021-05-20 17:36:21 -0700

    fix suffixes in the deployment script

--- deploy_to_pypi.sh
+++ deploy_to_pypi.sh
@@ -24,13 +24,13 @@ env
 # aws s3 cp --recursive s3://games-wheels/atari-py/${TRAVIS_TAG} wheelhouse/
 SUFFIXES="-manylinux_2_17_aarch64.manylinux2014_aarch64.whl -manylinux_2_17_x86_64.manylinux2014_x86_64.whl -win_amd64.whl -macosx_10_12_x86_64.whl"
 # SUFFIXES="-manylinux1_x86_64.whl"
-PY_VERS="-cp36-cp36m -cp37-cp37 -cp38-38m -cp39-39m"
+PY_VERS="-cp36-cp36m -cp37-cp37m -cp38-cp38m -cp39-cp39m"
 URLPREFIX="https://s3-${AWS_DEFAULT_REGION}.amazonaws.com/${S3_BUCKET}/${S3_PREFIX}atari-py/${TRAVIS_TAG}/atari_py-${TRAVIS_TAG}"
 
 cd wheelhouse
 for s in $SUFFIXES; do
   for p in $PY_VERS; do
-    curl -O -f ${URLPREFIX}${p}${s} || echo "${URLPREFIX}${p}{s} not found"
+    curl -O -f ${URLPREFIX}${p}${s} || echo "${URLPREFIX}${p}${s} not found"
   done;
 done
 
